33d176bddb94ada32bf1a872ead03e09edb91703
ishaque <ishaque@192.168.1.213>
2021-05-22 16:43:49 +0530

    changed here

--- Test/src/main/java/com/salman/staticmethod/B.java
+++ Test/src/main/java/com/salman/staticmethod/B.java
@@ -2,7 +2,7 @@ package com.salman.staticmethod;
 
 public class B {
 	public static void main(String[] args) {
-		System.out.println("hello this is call "+A.M1());
+		System.out.println("hello this is call to M1 "+A.M1());
 	}
 
 }
