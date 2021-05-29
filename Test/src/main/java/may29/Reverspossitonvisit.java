48dc5dcaf0fc8410ab7d4c4af2f0ce5e2acd94e0
ishaque <ishaque@192.168.1.218>
2021-05-29 15:57:24 +0530

    revers possiton of array 29-5

---
+++ Test/src/main/java/may29/Reverspossitonvisit.java
@@ -0,0 +1,11 @@
+package may29;
+
+public class Reverspossitonvisit {
+	public static void main(String[] args) {
+
+		int[] boys = { 2, 3, 4, 5, 6, 7, 8, 9 };
+		for (int i = boys.length - 1; i >= 0; i--) {
+			System.out.println(boys[i]);
+		}
+	}
+}
