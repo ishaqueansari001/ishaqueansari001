4be862c965930fd4946a8de7016e9b58bf1e1617
salman <nd@nd-PC>
2021-06-08 23:36:29 +0530

    order of precidence

---
+++ Test/src/main/java/evalutionOrderOfOpernds.java
@@ -0,0 +1,16 @@
+
+public class evalutionOrderOfOpernds {
+
+	public static void main(String[] args) {
+//there is no precidence for oprends  only precidency for opretar(+-*/etc)
+		System.out.println(m1(1) + m1(2) * m1(3) / m1(4) + m1(5) * m1(6));
+	}
+
+	public static int m1(int i) {
+//here we are returning int value thats way here we didn't used void
+		System.out.println(i);
+		return i;
+
+	}
+
+}
