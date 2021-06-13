8a1184df260f87464a648f1839f3672d9b073f45
ishaque <ishaque@192.168.1.210>
2021-06-13 15:12:04 +0530

    ishtiyaque practice

---
+++ Test/src/main/java/June12/Building.java
@@ -0,0 +1,20 @@
+package June12;
+
+public class Building {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		int[] salman = { 40, 80, 120, 'c' };
+		int[] salman1 = { 40, 80, 120, 'c' ,12};
+		m1(new int[]{1,2,3});
+		
+		m1(new int[]{1,2,3});
+		m1(new int[]{1,2,3});
+		m1(new int[]{1,2,3});
+	}
+	
+	
+	public static void m1(int[] arr) {
+		System.out.println("length of salman is "+ arr.length);
+	}
+}
