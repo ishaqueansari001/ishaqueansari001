14650f2ed1996f43dfd47fa8c1a20850b4e67fc0
Guest <Guest@ishaque-HP>
2021-07-04 16:47:54 +0530

    modifi in if elseif loop

--- Test/src/main/java/A_GfG_DesitionMakingLoop/If_Elseif_ElseLoop.java
+++ Test/src/main/java/A_GfG_DesitionMakingLoop/If_Elseif_ElseLoop.java
@@ -5,18 +5,19 @@ public class If_Elseif_ElseLoop {
 
 		int x = 18;
 		if (x == 10)// we can't use equal operator (x = _)
+		{
 			System.out.println("x is 10");
-		else if (x == 11)
+		} else if (x == 11) {
 			System.out.println("x is 11");
-		else if (x < 20)// here x is less then 20 whenever we put int x = 19,18,17..etc it will print
-						// (x is 20)
+		} else if (x < 20)// here x is less then 20 whenever we put int x = 19,18,17..etc it will print
+		// (x is 20)
+		{
 			System.out.println("x is 20");
-		else if (x == 21)
+		} else if (x == 21) {
 			System.out.println("x is 21");
-
-		else
+		} else {
 			System.out.println("x is not here");
-
+		}
 	}
 
 }
